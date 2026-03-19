1eda7f76239df91fdb328ecfadf6649f3de2bb54
David Corsalini <corsalini.david@gmail.com>
2026-03-19 14:59:38 +0100

    - fix: maven publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,27 +1,26 @@
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
-    kotlin("jvm") version "1.9.24"
-    kotlin("plugin.serialization") version "1.9.24"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.serialization") version "2.2.20"
+    id("com.vanniktech.maven.publish") version "0.36.0"
+    id("org.jetbrains.dokka") version "2.1.0"
 }
 
 ext {
     set("PUBLISH_GROUP_ID", "io.github.dadino.appinfoplugin")
     set("PUBLISH_VERSION", "3.0.0")
     set("PUBLISH_ARTIFACT_ID", "plugin")
-    set("ossrhToken", System.getProperty("ossrhToken"))
-    set("ossrhTokenPassword", System.getProperty("ossrhTokenPassword"))
-    set("sonatypeStagingProfileId", System.getProperty("sonatypeStagingProfileId"))
 }
 
 apply {
-    from("${rootProject.projectDir}/scripts/publish-mavencentral.gradle")
+    from("${rootProject.projectDir}/scripts/vannitech-mavencentral.gradle")
 }
 
 gradlePlugin {
     plugins {
         create("AppInfoPlugin") {
-            id = "com.appinfoplugin"
+            id = "io.github.dadino.appinfoplugin"
             implementationClass = "com.appinfoplugin.AppInfoPlugin"
         }
     }
@@ -30,7 +29,7 @@ group = "com.appinfoplugin"
 version = ext.properties["PUBLISH_VERSION"]!!
 
 java {
-    withJavadocJar()
+    // withJavadocJar()
     withSourcesJar()
 }
 
@@ -41,6 +40,6 @@ repositories {
 
 dependencies {
     implementation(kotlin("stdlib"))
-    compileOnly("com.android.tools.build:gradle-api:8.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    compileOnly("com.android.tools.build:gradle-api:8.13.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.10.0")
 }
